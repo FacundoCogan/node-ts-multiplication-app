c7a3b0a02b3c1b80824a93b5c222572f2665a940
Facundo <facundo_06@live.com.ar>
2024-01-21 02:47:05 -0300

    app final

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,10 +1,25 @@
+import exp from 'constants';
+import {ServerApp} from './presentation/server-app';
 
 
+describe('Test App.ts', () => {
 
-describe('App', () => {
+    test('should call Server.run with values', async() => {
+        
+        const serverRunMock = jest.fn();
+        ServerApp.run = serverRunMock; // Replace ServerApp.run with the mock function
+        process.argv=['node', 'app.ts', '-b', '10', '-l', '5', '-s', '-n', 'test-file', '-d', 'test-path'];
 
-    test('should be true', () => {
-        expect(true).toBe(true);
+        await import('./app');
+
+        expect(serverRunMock).toHaveBeenCalledWith({
+            base: 10, 
+            limit: 5, 
+            showTable: true,
+            name:'test-file', 
+            destination:'test-path'
+        });
+
     })
 
 });
